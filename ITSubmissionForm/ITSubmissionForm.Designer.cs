0ea750e183e2d159837bbafe091c57e18119e8fc
Hamberglar <wehttam.bassist@gmail.com>
2018-04-23 20:34:34 -0500

    Main Interface
    This is the full solution setup that includes the interface I've designed. Tonight or tomorrow, i'll upload what I've done for classes so far.

---
+++ ITSubmissionForm/ITSubmissionForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ITSubmissionForm
+{
+    partial class FormCheckIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPhones = new System.Windows.Forms.Label();
+            this.txtPhone1 = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPhone2 = new System.Windows.Forms.TextBox();
+            this.CalendarWidget = new System.Windows.Forms.MonthCalendar();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rtxtIssueDescription = new System.Windows.Forms.RichTextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(57, 7);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(391, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(3, 10);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(3, 36);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(58, 33);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(390, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // lblPhones
+            // 
+            this.lblPhones.AutoSize = true;
+            this.lblPhones.Location = new System.Drawing.Point(3, 62);
+            this.lblPhones.Name = "lblPhones";
+            this.lblPhones.Size = new System.Drawing.Size(38, 13);
+            this.lblPhones.TabIndex = 5;
+            this.lblPhones.Text = "Phone";
+            // 
+            // txtPhone1
+            // 
+            this.txtPhone1.Location = new System.Drawing.Point(57, 59);
+            this.txtPhone1.Name = "txtPhone1";
+            this.txtPhone1.Size = new System.Drawing.Size(188, 20);
+            this.txtPhone1.TabIndex = 4;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(57, 85);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(391, 20);
+            this.txtAddress.TabIndex = 6;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(3, 88);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(45, 13);
+            this.lblAddress.TabIndex = 12;
+            this.lblAddress.Text = "Address";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(57, 107);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(391, 20);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(3, 112);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 10;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPhone2
+            // 
+            this.txtPhone2.Location = new System.Drawing.Point(251, 59);
+            this.txtPhone2.Name = "txtPhone2";
+            this.txtPhone2.Size = new System.Drawing.Size(197, 20);
+            this.txtPhone2.TabIndex = 9;
+            // 
+            // CalendarWidget
+            // 
+            this.CalendarWidget.Location = new System.Drawing.Point(12, 154);
+            this.CalendarWidget.Name = "CalendarWidget";
+            this.CalendarWidget.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblName);
+            this.panel2.Controls.Add(this.txtName);
+            this.panel2.Controls.Add(this.txtEmail);
+            this.panel2.Controls.Add(this.lblAddress);
+            this.panel2.Controls.Add(this.lblEmail);
+            this.panel2.Controls.Add(this.txtPassword);
+            this.panel2.Controls.Add(this.txtPhone1);
+            this.panel2.Controls.Add(this.lblPassword);
+            this.panel2.Controls.Add(this.lblPhones);
+            this.panel2.Controls.Add(this.txtPhone2);
+            this.panel2.Controls.Add(this.txtAddress);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(451, 136);
+            this.panel2.TabIndex = 19;
+            // 
+            // rtxtIssueDescription
+            // 
+            this.rtxtIssueDescription.Location = new System.Drawing.Point(251, 154);
+            this.rtxtIssueDescription.Name = "rtxtIssueDescription";
+            this.rtxtIssueDescription.Size = new System.Drawing.Size(395, 198);
+            this.rtxtIssueDescription.TabIndex = 20;
+            this.rtxtIssueDescription.Text = "";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Power Cord",
+            "Bag/Case",
+            "External Disk",
+            "Other"});
+            this.checkedListBox1.Location = new System.Drawing.Point(469, 9);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(177, 64);
+            this.checkedListBox1.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(470, 80);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(176, 68);
+            this.textBox7.TabIndex = 22;
+            this.textBox7.Text = "Other";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(12, 329);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(106, 23);
+            this.btnSubmit.TabIndex = 23;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(133, 328);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(106, 23);
+            this.btnClear.TabIndex = 24;
+            this.btnClear.Text = "Cancel";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // formCheckIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 364);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.rtxtIssueDescription);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.CalendarWidget);
+            this.Name = "formCheckIn";
+            this.Text = "Check In";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPhones;
+        private System.Windows.Forms.TextBox txtPhone1;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPhone2;
+        private System.Windows.Forms.MonthCalendar CalendarWidget;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RichTextBox rtxtIssueDescription;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
