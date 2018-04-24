8de2e64aab1ac3360fe3c01a9b1bacceda63a031
Hamberglar <wehttam.bassist@gmail.com>
2018-04-23 21:44:12 -0500

    Added 2 classes with constructors and get/set properties.

---
+++ ITSubmissionForm/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Inventory
+    {
+        private string _ComputerBrand { get; set; }
+        private string _ComputerModel { get; set; }
+        private bool _Bag { get; set; }
+        private bool _PowerCord { get; set; }
+        private bool _ExtDrive { get; set; }
+        private bool _CompactDiscs { get; set; }
+        private string _Other { get; set; }
+
+        public Inventory(string brand, string model)
+        {
+            this._ComputerBrand = brand;
+            this._ComputerModel = model;
+        }
+    }
+}
--- ITSubmissionForm/User.cs
+++ ITSubmissionForm/User.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace ITSubmissionForm
 {
-    class User
+    public class User
     {
-        
+        private string _Name { get; set; }
+        private string _Phone1 { get; set; }
+        private string _Phone2 { get; set; }
+        private string _Email { get; set; }
+        private string _Address1 { get; set; }
+        private string _Address2 { get; set; }
+        private string DefaultUsername;
+
+        /*public string Name { get => _Name; set => _Name = value; }
+        public string Phone1 { get => _Phone1; set => _Phone1 = value; }
+        public string Phone2 { get => _Phone2; set => _Phone2 = value; }
+        public string Email { get => _Email; set => _Email = value; }
+        public string Address1 { get => _Address1; set => _Address1 = value; }
+        public string Address2 { get => _Address2; set => _Address2 = value; }*/
+
+        public User()
+        {
+            new User(DefaultUsername);
+        }
+
+        public User(string UserName)
+        {
+            this._Name = UserName;
+        }
     }
 }
