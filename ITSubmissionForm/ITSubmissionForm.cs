3dbac3092055f72b43422516d62c4d1f5f77573e
Matthew Berg <meberg@pluto.dsu.edu>
2018-04-26 17:46:38 -0500

    More refinements

--- ITSubmissionForm/ITSubmissionForm.cs
+++ ITSubmissionForm/ITSubmissionForm.cs
@@ -19,18 +19,17 @@ namespace ITSubmissionForm
 
         private void BtnSubmit_Click(object sender, EventArgs e)
         {
+            //initialize our instances; we should only need one of each;
             Case PresentCase = new Case(rtxtIssueDescription.Text);
             Inventory PresentInventory = new Inventory(txtBrand.Text, txtModel.Text);
             User PresentUser = new User(txtName.Text);
 
+            //record all the entries for the case class
             PresentCase.Tech = txtTech.Text;
             PresentCase.Date = Convert.ToString(CalendarWidget.SelectionStart);
             PresentCase.Time = txtTime.Text;
 
-            txtTech.Text = "";
-            txtTime.Text = "";
-            rtxtIssueDescription.Text = "";
-
+            //record all the entires for the inventory class
             PresentInventory.Bag = chkBag.Checked;
             PresentInventory.PowerCord = chkPowerCord.Checked;
             PresentInventory.UsbDrive = chkExternal.Checked;
@@ -39,29 +38,14 @@ namespace ITSubmissionForm
             PresentInventory.Keyboard = chkKeyboard.Checked;
             PresentInventory.Other = txtOther.Text;
 
-            chkBag.Checked = false;
-            chkPowerCord.Checked = false;
-            chkExternal.Checked = false;
-            chkDiscs.Checked = false;
-            chkMouse.Checked = false;
-            chkKeyboard.Checked = false;
-            txtOther.Text = "";
-            txtBrand.Text = "";
-            txtModel.Text = "";
-
+            //record all the entris for the user class
             PresentUser.Phone1 = txtPhone1.Text;
             PresentUser.Phone2 = txtPhone2.Text;
             PresentUser.Email = txtEmail.Text;
             PresentUser.Address = txtAddress.Text;
             PresentUser.Password = txtPassword.Text;
 
-            txtName.Text = "";
-            txtPhone1.Text = "";
-            txtPhone2.Text = "";
-            txtEmail.Text = "";
-            txtAddress.Text = "";
-            txtPassword.Text = "";
-
+            //the following is just a placeholder for further API/database integration. This just lets you see that it's working, essentially. Not intended for long term use.
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\temp\outputexample.txt", true))
             {
                 file.WriteLine("Case ID: " + Convert.ToString(PresentCase.CaseID));
@@ -104,10 +88,29 @@ namespace ITSubmissionForm
                 }
                 file.WriteLine("\n");
             }
+
+            //submit also clears, so the next user can enter their information
+            ClearEntries();
         }
 
         private void BtnClear_Click(object sender, EventArgs e)
         {
+            var confirmResult = MessageBox.Show("Are you sure you want to clear all entries?",
+                                     "Confirm Clear",
+                                     MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes)
+            {
+                ClearEntries();
+            }
+            else
+            {
+                //don't do anything
+            }
+        }
+
+        private void ClearEntries()
+        {
+            //this should clear all the entries
             txtTech.Text = "";
             txtTime.Text = "";
             rtxtIssueDescription.Text = "";
@@ -117,7 +120,7 @@ namespace ITSubmissionForm
             chkDiscs.Checked = false;
             chkMouse.Checked = false;
             chkKeyboard.Checked = false;
-            txtOther.Text = "";
+            txtOther.Text = "Other";
             txtBrand.Text = "";
             txtModel.Text = "";
             txtName.Text = "";
